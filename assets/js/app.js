7dc5190901bc4c4bfbc8121dd1bc84609b206f69
Maripin <m.paz.thompson@gmail.com>
2018-01-23 11:33:53 -0300

    Actualizando html, css y js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,26 @@
-objCalculator = {
-  addNumber: function(a, b){
-    return a + b;
-  },
+/* Función Desplegable */
 
-  substractNumber: function(a, b){
-    return a - b;
-  },
-
-  multiplyNumber: function(a, b){
-    return a * b;
-  },
-
-  divideNumber: function(a, b){
-    return a / b;
-  }
+jQuery.fn.animateAuto = function(prop, speed, callback){
+   var elem, height, width;
+   return this.each(function(i, el){
+      el = jQuery(el), elem = el.clone().css({"height":"auto","width":"auto"}).appendTo("body");
+      height = elem.css("height"),
+      width = elem.css("width"),
+      elem.remove();
+      if(prop === "height")
+         el.animate({"height":height}, speed, callback);
+      else if(prop === "width")
+         el.animate({"width":width}, speed, callback);  
+      else if(prop === "both")
+         el.animate({"width":width,"height":height}, speed, callback);
+   });  
 }
-
-// Estamos validando si nos encontramos en un ambiente de node
-// El "module" solo existe en node asi que para constatar que estamos en un ambiente node verificamos que la variable no sea indefinida.
-// Si estamos en un ambiente Node importamos el código para poder usarlo en nuestras pruebas
-if ( typeof module != 'undefined' && module.exports ) {
-	module.exports = objCalculator;
-}
+$(window).ready(function(){
+   $('h2').click(function(){
+      if($(this).next().hasClass('desplegado')){
+         $(this).next().removeClass('desplegado').animate({height:0},500);
+      }else{
+         $(this).next().addClass('desplegado').animateAuto("height",500);
+      }
+   })
+})
