ccfe06358c43111e8d458e957f796e87511e3a99
Maripin <m.paz.thompson@gmail.com>
2018-01-24 16:06:13 -0300

    Agregando funcionalidad de movimiento a la derecha, cambiando ids, etc

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 /* Función Desplegable */
 
-jQuery.fn.deploy = function(prop, speed, callback){
+/*jQuery.fn.deploy = function(prop, speed, callback){
    var elem, height, width;
    return this.each(function(i, el){
       el = jQuery(el), elem = el.clone().css({"height":"auto","width":"auto"}).appendTo("body");
@@ -10,10 +10,10 @@ jQuery.fn.deploy = function(prop, speed, callback){
       if(prop === "height")
          el.animate({"height":height}, speed, callback);
       else if(prop === "width")
-         el.animate({"width":width}, speed, callback);  
+         el.animate({"width":width}, speed, callback);
       else if(prop === "both")
          el.animate({"width":width,"height":height}, speed, callback);
-   });  
+   });
 }
 $(window).ready(function(){
    $('.down').mouseover(function(){
@@ -22,5 +22,19 @@ $(window).ready(function(){
       }else{
          $('.plegable').addClass('desplegado').deploy("height",500);
       }
-   })
+   })*/
+
+$(document).ready(function(){
+
+  /* Función para movimiento de div de izquierda al centro */
+  $('#divToRight').parent().mouseover(function(){
+    console.log($('#divToRight').parent().width());
+    /* Añadiendo estilo al div que tendrá moviento, es importante que esté en position relative. */
+    $('#divToRight').css("position", "relative");
+    /* Para que quede centrado, la función animate toma el ancho del div padre del div que se moverá y la divide en 2, luego le resta el ancho del div que se moverá dividido en 2 */
+    $('#divToRight').animate({left: $('#divToRight').parent().width() / 2 - $('#divToRight').width() / 2});
+   });
+
+
 })
+
