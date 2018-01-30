dd0dec5e2c82a0a68729b6d9f5aac1b1d88c9a32
hobilin <paulibareyes@gmail.com>
2018-01-30 18:34:34 -0300

    cambiando ids por this para que funcione espeficicamente en el div llamado

--- src/addictive.js
+++ src/addictive.js
@@ -25,12 +25,12 @@ addictive.toDown = function() {
       });
     };
 
-  const divToDown = $('.plegable');
+  const divToDown = $(this);
   const cordDown = divToDown.offset().top - 400;
 
     $(window).scroll(function() {
       if (window.scrollY >= cordDown) {
-        $('.plegable').addClass('desplegado').down('height', 500);
+        $(divToDown).addClass('desplegado').down('height', 500);
       }
     });
     };
@@ -43,15 +43,15 @@ addictive.toDown();
 addictive.toRight = function() {
    (function($) {
     $.fn.slideRight = function() {
-      const divToRight = $('#divToRight');
+      const divToRight = $(this);
     const cordRight = divToRight.offset().top - 200;
     $(window).scroll(function() {
       if (window.scrollY >= cordRight) {
-        $('#divToRight').css('position', 'relative');
+        $(divToRight).css('position', 'relative');
         /* Para que quede centrado, la función animate toma el ancho del div padre del div que se moverá y la divide en 2
         *luego le resta el ancho del div que se moverá dividido en 2
         */
-        $('#divToRight').animate({left: $('#divToRight').parent().width() / 2 - $('#divToRight').width() / 2});
+        $(divToRight).animate({left: $(divToRight).parent().width() / 2 - $('#divToRight').width() / 2});
       };
     });
     };
@@ -62,18 +62,18 @@ addictive.toRight();
 addictive.toLeft = function() {
      (function($) {
     $.fn.slideLeft = function() {
-  const divToLeft = $('#divToLeft');
+  const divToLeft = $(this);
     const cordLeft = divToLeft.offset().top - 200;
     $(window).scroll(function() {
       if (window.scrollY >= cordLeft) {
         console.log('somos iguales');
 
-        $('#divToLeft').css('position', 'relative');
+        $(divToLeft).css('position', 'relative');
         /* Para que quede centrado, la función animate toma el ancho del div padre del div que se moverá y la divide en 2, luego le resta el ancho del div que se moverá dividido en 2 */
-        $('#divToLeft').animate({right: $('#divToLeft').parent().width() / 2 - $('#divToLeft').width() / 2});
+        $(divToLeft).animate({right: $(divToLeft).parent().width() / 2 - $(divToLeft).width() / 2});
       };
     });
-  };
+    };
 }($));
   };
   addictive.toLeft();
