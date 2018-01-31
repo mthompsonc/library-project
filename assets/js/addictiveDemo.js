dade69ad3bec8266dacd62bf1747928d52fb4df6
hobilin <paulibareyes@gmail.com>
2018-01-31 18:57:47 -0300

    arreglando archivos

--- assets/js/addictiveDemo.js
+++ assets/js/addictiveDemo.js
@@ -3,43 +3,6 @@
 const addictive = {};
 
 
-addictive.toDown = function() {
-  (function($) {
-    $.fn.deploy = function() {
-  $.fn.down = function(prop, speed, callback) {
-      var elem, height, width;
-      return this.each(function(i, el) {
-        el = $(el), elem = el.clone().css({
-          'height': 'auto',
-          'width': 'auto'}).appendTo('body');
-        height = elem.css('height'),
-        width = elem.css('width'),
-        elem.remove();
-        if (prop === height)
-          el.animate({'height': height}, speed, callback);
-        else if (prop === 'width')
-          el.animate({'width': width}, speed, callback);
-        else if (prop === 'both')
-          el.animate({'width': width,
-            'height': height}, speed, callback);
-      });
-    };
-
-  const divToDown = $(this);
-  const cordDown = divToDown.offset().top - 400;
-
-    $(window).scroll(function() {
-      if (window.scrollY >= cordDown) {
-        $(divToDown).addClass('desplegado').down('height', 500);
-      }
-    });
-    };
-}($));
-  };
-
-addictive.toDown();
-
- 
 addictive.toRight = function() {
    (function($) {
     $.fn.slideRight = function() {
@@ -51,7 +14,7 @@ addictive.toRight = function() {
         /* Para que quede centrado, la función animate toma el ancho del div padre del div que se moverá y la divide en 2
         *luego le resta el ancho del div que se moverá dividido en 2
         */
-        $(divToRight).animate({left: $(divToRight).parent().width() / 2 - $('#divToRight').width() / 2});
+        $(divToRight).animate({left: $(divToRight).parent().width() / 2 - $(divToRight).width() / 2});
       };
     });
     };
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,4 @@
 $(document).ready (function (){
  $('#divToRight').slideRight();
   $('#divToLeft').slideLeft();
-    $('#divToLeft2').slideLeft();
-  $('.plegable').deploy();
 });
